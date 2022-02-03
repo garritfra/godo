0865e232a3e992cb57ffa363a794516b2b4c9d18
Garrit Franke <garrit@slashdev.space>
2022-02-03 09:16:02 +0100

    server: init

--- godo.go
+++ godo.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"github.com/garritfra/godo/server"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	r := server.SetupRouter()
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
 }
